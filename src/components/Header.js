df1307ed4f3307619bcd268b5d98c9d271fadcfb
Lee <dltpdnd1234@gmail.com>
2021-04-25 19:50:09 -0500

    toggle close and add

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,11 @@ import React from 'react'
 import PropTypes  from 'prop-types'
 import Button from './Button'
 
-const onClick = () =>{
-  console.log('click');
-}
-
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
   return (
     <header className='header'>
       <h1 style={lee}>{title}</h1>
-      <Button color='green' text='sewoong' onClick={onClick}/>
+      <Button color={showAdd ? 'red' :'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
       
     </header>
   )
