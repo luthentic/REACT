0f2b1fee857fca19f65e0e04dfe2230e8564430c
Lee <dltpdnd1234@gmail.com>
2021-06-15 22:29:57 -0500

    usestate review

--- reactpr/src/App.js
+++ reactpr/src/App.js
@@ -1,23 +1,30 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let lee ='LEE'
+let css ={color:'red', fontSize:'30px'}
+
 function App() {
+
+  let [LEE1,LEE2] = useState(['LEEE1', 'LEEE2','LEEE3']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div style={ css }>PPLLLPLP</div>
+      </div>
+      <div className="list">
+        <h3>{LEE1[0] }</h3>
+        <p>FEB 13</p>
+        <hr/>
+        <h3>{LEE1[1] }</h3>
+        <p>FEB 13</p>
+        <hr/>
+        <h3>{LEE1[2] }</h3>
+        <p>FEB 13</p>
+        <hr/>
+      </div>
     </div>
   );
 }
