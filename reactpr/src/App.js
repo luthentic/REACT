1b93dc656baf480b18ccd18d2223dfaebff377a5
Lee <dltpdnd1234@gmail.com>
2021-06-16 01:43:00 -0500

    TNN components

--- reactpr/src/App.js
+++ reactpr/src/App.js
@@ -1,55 +1,21 @@
-import React,{Fragment, useState} from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
-import { func } from 'prop-types';
+import Navbar from './Navbar';
+import Home from './Home';
 
-let lee ='LEE'
-let css ={color:'red', fontSize:'30px'}
 
 function App() {
-
-  let [LEE1,LEE2] = useState(['LEEE1', 'LEEE2','LEEE3']);
-  let [GOOD1,GOOD2] = useState(0)
-
-  function Change(){
-    let newArray = [...LEE1]
-    newArray[0] = 'KIM1'
-    LEE2(newArray)
-  }
+  
   return (
     <div className="App">
-      <div className='black-nav'>
-        <div style={ css }>PPLLLPLP</div>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
-      <button onClick={Change}>PPPPP</button> 
-      <div className="list">
-        <h3>{LEE1[0] } <span onClick={()=>{GOOD2(GOOD1 + 1)}}>👍</span> {GOOD1} </h3> {/* onclick has only function  */}
-        <p>FEB 13</p>
-        <hr/>
-        <h3>{LEE1[1] }</h3>
-        <p>FEB 13</p>
-        <hr/>
-        <h3>{LEE1[2] }</h3>
-        <p>FEB 13</p>
-        <hr/>
-      </div>
-    <Modal/>
-
     </div>
   );
 }
 // component must be capital
-function Modal(){
-  return(
-    // Fragment
-  <>
-    <div className="modal">
-    <h2>TITLE</h2>
-    <p>DAY</p>
-    <p>DETAIL</p>
-  </div>
-  </>
-  )
-}
+// array and object must deep copy reference data type
 
 export default App;
