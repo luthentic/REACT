14e52d1173c6b37420c0decda6c61c482189fa63
Lee <dltpdnd1234@gmail.com>
2021-06-17 01:18:15 -0500

    click EVENTS

--- reactpr/src/Home.js
+++ reactpr/src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+  const handleClcik = (e) =>{
+    console.log('HELLO LEE',e);
+  }
+  const again = (name,e) =>{
+    console.log('HELLO' + name ,e.target);
+  }
+
   return ( 
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClcik}>Click ME</button>
+      <button onClick={(e)=> again('KIM',e)}>Click ME</button>
     </div>
   );
 }
