c09ac1fb4c5852f67d6b744e7afd4c1e2dbd7ad4
Lee <dltpdnd1234@gmail.com>
2021-06-17 22:18:24 -0500

    Props

--- reactpr/src/Home.js
+++ reactpr/src/Home.js
@@ -1,18 +1,16 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   
-  const [blog,setBlog] = useState([
+  const [blogs,setBlogs] = useState([
     {title:'MY LEE WEB1', body:'LPOA PLPOLQOOP', author:'LEE1', id:1},
     {title:'MY LEE WEB2', body:'LPOA PLPOLQOOP', author:'LEE2', id:2},
     {title:'MY LEE WEB3', body:'LPOA PLPOLQOOP', author:'LEE3', id:3}
   ])
   return ( 
     <div className="home">
-      <h2>Home Page</h2>
-      <p>{name} is {age}</p>
-      <button onClick={handleClcik}>Click ME</button>
-      
+      <BlogList blogs={blogs} title='All Blogs'/>
     </div>
   );
 }
