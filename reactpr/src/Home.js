925eb90ef27f9b0dde394fef47b0f94d9fd6bbac
Lee <dltpdnd1234@gmail.com>
2021-06-17 21:59:26 -0500

    Outputiing

--- reactpr/src/Home.js
+++ reactpr/src/Home.js
@@ -2,15 +2,11 @@ import { useState } from 'react';
 
 const Home = () => {
   
-  const [name,setName] = useState('LEEEE')
-  const [age,setAge] = useState(20)
-
-  const handleClcik = (e) =>{
-    setName('KOM')
-    setAge(30)
-  }
-
-
+  const [blog,setBlog] = useState([
+    {title:'MY LEE WEB1', body:'LPOA PLPOLQOOP', author:'LEE1', id:1},
+    {title:'MY LEE WEB2', body:'LPOA PLPOLQOOP', author:'LEE2', id:2},
+    {title:'MY LEE WEB3', body:'LPOA PLPOLQOOP', author:'LEE3', id:3}
+  ])
   return ( 
     <div className="home">
       <h2>Home Page</h2>
