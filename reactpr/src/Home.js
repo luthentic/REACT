4713cded97cea2a8f70c30bf6289b43b2bd62685
Lee <dltpdnd1234@gmail.com>
2021-06-17 22:51:10 -0500

    Delete props

--- reactpr/src/Home.js
+++ reactpr/src/Home.js
@@ -8,9 +8,15 @@ const Home = () => {
     {title:'MY LEE WEB2', body:'LPOA PLPOLQOOP', author:'LEE2', id:2},
     {title:'MY LEE WEB3', body:'LPOA PLPOLQOOP', author:'LEE3', id:3}
   ])
+
+  const handleDelete = (id) =>{
+    const newBLogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBLogs)
+  }
+
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs'/>
+      <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
     </div>
   );
 }
