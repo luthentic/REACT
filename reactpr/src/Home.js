82980c957b7597611d836377a740d5ce732ace6c
Lee <dltpdnd1234@gmail.com>
2021-06-18 23:33:21 -0500

    fetch error handling

--- reactpr/src/Home.js
+++ reactpr/src/Home.js
@@ -3,13 +3,9 @@ import BlogList from './BlogList';
 
 const Home = () => {
   
-  const [blogs,setBlogs] = useState([
-    {title:'MY LEE WEB1', body:'LPOA PLPOLQOOP', author:'LEE1', id:1},
-    {title:'MY LEE WEB2', body:'LPOA PLPOLQOOP', author:'LEE2', id:2},
-    {title:'MY LEE WEB3', body:'LPOA PLPOLQOOP', author:'LEE3', id:3}
-  ])
-
-  const [name,setName] = useState('LEE')
+  const [blogs,setBlogs] = useState(null)
+  const [isPending,setIsPending] = useState(true)
+  const [error,setError] = useState(null)
 
   const handleDelete = (id) =>{
     const newBLogs = blogs.filter(blog => blog.id !== id)
@@ -17,17 +13,29 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res =>{
-        return res.json()
-      })
-      .then(data =>{
-        setBlogs(data)
-      })
+    setTimeout(()=>{
+      fetch('http://localhost:8000/blogs')
+        .then(res =>{
+          if(!res.ok){
+            throw Error('Could not fetch the data')
+          }
+          return res.json()
+        })
+        .then(data =>{
+          setBlogs(data)
+          setIsPending(false)
+        })
+        .catch(err =>{
+          setIsPending(false)
+          setError(err.message)
+        })
+      },1000)
   }, [])
 
   return ( 
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>}
     </div>
   );
