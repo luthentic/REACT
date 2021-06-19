b137b7ee42fc5fc8ac7f23191d73e5e5357f9c58
Lee <dltpdnd1234@gmail.com>
2021-06-19 00:56:19 -0500

    router Link

--- reactpr/src/Navbar.js
+++ reactpr/src/Navbar.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return ( 
     <nav className="navbar">
       <h1>The LEE BLOG</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{color:'white', backgroundColor:'#f1356d', borderRadius:'8px'}}>New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create" >New Blog</Link>
+        {/* style={{color:'white', backgroundColor:'#f1356d', borderRadius:'8px'}} */}
       </div>
     </nav>  
   );
