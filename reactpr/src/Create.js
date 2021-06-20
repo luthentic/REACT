d6e7e72a4769ff093a1c0a18ff201b0e4c28c4b7
Lee <dltpdnd1234@gmail.com>
2021-06-19 21:46:36 -0500

    POST Request

--- reactpr/src/Create.js
+++ reactpr/src/Create.js
@@ -4,10 +4,24 @@ const Create = () => {
   const [title,setTitle] = useState('')
   const [body,setBody] = useState('')
   const [author,setAuthor] = useState('')
+  const [isPending,setIsPending] = useState(false)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const blog = {title, body, author }
+
+    fetch('http://localhost:8000/blogs',{
+      method:'POST',
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(blog)
+    }).then(()=>{
+      console.log('New Blog Add');
+    })
+  }
   return ( 
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog Title:</label>
         <input
           type="text"
@@ -29,7 +43,8 @@ const Create = () => {
           <option value='mario'>Mario</option>
           <option value='yoshi'>yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Adding Blog...</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
